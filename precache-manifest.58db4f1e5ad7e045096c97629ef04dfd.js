97a36b3bc3396736a4e138e5d90b85ff8638897d
Chris Woolf <CWoolf@penguinrandomhouse.co.uk>
2019-09-16 09:08:01 +0100

    Updates

---
+++ precache-manifest.58db4f1e5ad7e045096c97629ef04dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44024ef80ac425e0b01",
+    "url": "/taskRunnerApp/taskRunnerApp.github.io.git/static/js/runtime~main.b44024ef.js"
+  },
+  {
+    "revision": "d5575c87c725bfe2a16f",
+    "url": "/taskRunnerApp/taskRunnerApp.github.io.git/static/js/main.d5575c87.chunk.js"
+  },
+  {
+    "revision": "8e655a0395c1667f6b9a",
+    "url": "/taskRunnerApp/taskRunnerApp.github.io.git/static/js/1.8e655a03.chunk.js"
+  },
+  {
+    "revision": "d5575c87c725bfe2a16f",
+    "url": "/taskRunnerApp/taskRunnerApp.github.io.git/static/css/main.d6c85e21.chunk.css"
+  },
+  {
+    "revision": "777756879d888aef764f2f544c454c67",
+    "url": "/taskRunnerApp/taskRunnerApp.github.io.git/index.html"
+  }
+];
